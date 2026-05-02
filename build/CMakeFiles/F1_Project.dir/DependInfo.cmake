2e53acb1849f2bbae2b1c98b6938ea8740471f0b
ArtuurH-2364347 <Artuur.heidbuchel@student.uhasselt.be>
2026-05-02 12:40:51 +0200

    push without gitignore

---
+++ build/CMakeFiles/F1_Project.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ASCII/Project-Computer-Graphics-1/glad.c" "CMakeFiles/F1_Project.dir/glad.c.o" "gcc" "CMakeFiles/F1_Project.dir/glad.c.o.d"
+  "/home/ASCII/Project-Computer-Graphics-1/src/main.cpp" "CMakeFiles/F1_Project.dir/src/main.cpp.o" "gcc" "CMakeFiles/F1_Project.dir/src/main.cpp.o.d"
+  "" "F1_Project" "gcc" "CMakeFiles/F1_Project.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/F1_Project.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/F1_Project.dir/link.d"
+  "CMakeFiles/F1_Project.dir/glad.c.o"
+  "CMakeFiles/F1_Project.dir/glad.c.o.d"
+  "CMakeFiles/F1_Project.dir/src/main.cpp.o"
+  "CMakeFiles/F1_Project.dir/src/main.cpp.o.d"
+  "F1_Project"
+  "F1_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/F1_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
